405e7651a58072b05ad36a54d05a3771948c1fb2
maxim <maximoksiuta@yandex.ru>
2023-09-25 15:45:44 +0300

    initial commit

---
+++ app/src/main/java/com/moscow/projects/sport/smart/equipment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.moscow.projects.sport.smart.equipment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
